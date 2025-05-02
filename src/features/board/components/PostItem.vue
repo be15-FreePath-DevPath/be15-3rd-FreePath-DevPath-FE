7fd87532f8e14b4fc22cbf5a61dd03a6132a4d67
splguyjr <splguyjr@naver.com>
2025-05-02 14:55:53 +0900

    [Feat #21] 게시글 목록 조회 페이지 구현, Toast 라이브러리 기능 추가

---
+++ src/features/board/components/PostItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="post-item">
+    <div class="item title">
+      {{ text }}
+    </div>
+    <div class="item date">
+      <img src="@/assets/images/board/CalendarIcon.png" alt="Calendar Icon" class="icon" />
+      {{ date }}
+    </div>
+    <div class="item user">
+      {{ user }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: { type: String, required: true },
+  date: { type: String, required: true },
+  user: { type: String, required: true },
+})
+</script>
+
+<style scoped>
+.post-item {
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr;
+  align-items: center;
+  width: auto;
+  height: 65px;
+  border-bottom: 1px solid #0000000d;
+  font-size: 14px;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 0 8px;
+  color: #000;
+  font-weight: 400;
+}
+
+.title {
+  justify-content: flex-start;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.date {
+  font-size: 12px;
+  color: #1c1c1c;
+  justify-content: center;
+}
+
+.user {
+  justify-content: center;
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+}
+</style>
---
+++ src/features/board/components/PostListHeaderBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post-header">
+    <div class="header-item title">게시글 제목</div>
+    <div class="header-item date">작성일자</div>
+    <div class="header-item author">작성자</div>
+  </div>
+</template>
+
+<style scoped>
+.post-header {
+  display: grid;
+  grid-template-columns: 2fr 1fr 1fr;
+  border-bottom: 1px solid #dcdcdc;
+  color: #aaa;
+  font-size: 14px;
+  text-align: center;
+  padding: 8px 0;
+  width: auto;
+}
+
+.header-item {
+  padding: 4px 8px;
+}
+</style>
