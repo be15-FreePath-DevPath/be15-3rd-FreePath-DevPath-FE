cd9c3d970010a0c3e0caa065bf8e326786877b77
nineeko <gkxm1013@naver.com>
2025-05-04 22:18:45 +0900

    [feature #43] 마이페이지 북마크, 좋아요 게시글 모아보기 기능

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import {adminRoutes} from "@/features/admin/router.js";
 import LayoutDefault from "@/components/layout/LayoutDefault.vue";
 import {itNewsRoutes} from "@/features/admin/ItNews/router.js";
 import {boardRoutes} from "@/features/board/router.js";
+import {adminCsQuizRoutes} from "@/features/admin/csquiz/router.js";
+import {mypageRoutes} from "@/features/mypage/router.js";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -19,7 +21,9 @@ const router = createRouter({
                 ...csquizRoutes,
                 ...adminRoutes,
                 ...itNewsRoutes,
-                ...boardRoutes
+                ...boardRoutes,
+                ...adminCsQuizRoutes,
+                ...mypageRoutes
             ]
         }
     ]
