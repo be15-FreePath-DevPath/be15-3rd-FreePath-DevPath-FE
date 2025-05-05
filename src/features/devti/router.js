e56cd59466653fd4df9e2def6c56d119391cc630
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-06 00:01:04 +0900

    [feat #62] DevTI 테스트 응시 화면

--- src/features/devti/router.js
+++ src/features/devti/router.js
@@ -1,9 +1,15 @@
 import DevtiIntroView from "@/features/devti/views/DevtiIntroView.vue";
+import DevtiTestView from "@/features/devti/views/DevtiTestView.vue";
 
 export const devtiRoutes = [
     {
         path: '/mypage/devti/solve',
         name: 'DevtiIntro',
         component: DevtiIntroView
+    },
+    {
+        path: '/mypage/devti/test',
+        name: 'DevtiTest',
+        component: DevtiTestView
     }
 ];
--- src/features/devti/views/DevtiIntroView.vue
+++ src/features/devti/views/DevtiIntroView.vue
@@ -4,7 +4,7 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const startTest = () => {
-  router.push('/devti/test')
+  router.push('/mypage/devti/test')
 }
 </script>
 
