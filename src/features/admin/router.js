4c781a8e97a5093f4e22f79b2cf4843a3c5f5cb1
nineeko <gkxm1013@naver.com>
2025-05-06 19:37:05 +0900

    [feature #63]axios 관련 api router 파일 수정

--- src/features/admin/router.js
+++ src/features/admin/router.js
@@ -8,21 +8,13 @@ export const adminRoutes = [
     {
         path: '/admin',
         name: 'AdminMain',
-        component: AdminMainView
-    },
-    {
-        path: '/admin/csquiz/write',
-        name: 'AdminCsQuizWrite',
-        component: CsQuizWriteView
-    },
-    {
-        path: '/admin/csquiz/:csquizId',
-        name: 'AdminCsQuizDetail',
-        component: CsQuizDetailView
+        component: AdminMainView,
+        meta: { requiresAdmin: true }
     },
     {
         path: '/report/check/:id',  // 경로 추가
         name: 'ReportDetail',
         component: ReportDetailView,  // 해당 컴포넌트를 라우트에 연결
+        meta: { requiresAdmin: true }
     }
 ]
--- src/features/mypage/like/api.js
+++ src/features/mypage/like/api.js
@@ -1,15 +1,6 @@
-import axios from "axios";
+import api from "@/api/axios.js";
 
-const api = axios.create({
-    baseURL: "http://localhost:8080"
-});
-api.interceptors.request.use(config => {
-    const token = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzQ2NTIwOTE2LCJleHAiOjE3NDY1Mzg5MTZ9.JJ9M_TzNfKLtVTv13bsYJmVyklctZ6mziCXI4Q9fpF0WMB5kyHSbFR5I-ZzAHHdaEM4lta1kxzaY7wYHcSQTxw`;
-    // const token = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzQ2MzYyOTA2LCJleHAiOjE3NDYzODA5MDZ9.7-F3vIjHwouA1CzqtQk8aWcgSGj8ztIKPR610S95y8bSTi77kPRtnySP0TO38cJ4xQvzNGZXThrrtNYgE0VQKw`;
 
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
 // 좋아요한 게시글 목록 조회
 export const getLikedList = (params) => {
     return api.get('/mypage/like', { params });
