8897e6cedf38799ca97a528d7994896c01ce7ce5
nineeko <gkxm1013@naver.com>
2025-05-07 11:50:03 +0900

    [feature #95] 마이페이지 모아보기 작성한 게시글, 신고된 게시글 조회 부분 추가

--- src/features/mypage/components/MyPageContentList.vue
+++ src/features/mypage/components/MyPageContentList.vue
@@ -3,6 +3,8 @@ import BookmarkList from "@/features/mypage/bookmark/components/BookmarkList.vue
 import LikeList from "@/features/mypage/like/components/LikeList.vue";
 import CommentList from "@/features/mypage/comment/components/CommentList.vue";
 import ReportCommentList from "@/features/mypage/comment/components/ReportCommentList.vue";
+import ReportedPostList from "@/features/mypage/reportedpost/components/ReportedPostList.vue";
+import WritePostList from "@/features/mypage/writepost/components/WritePostList.vue";
 
 const props = defineProps(['selectedMain', 'selectedSub'])
 </script>
@@ -10,11 +12,17 @@ const props = defineProps(['selectedMain', 'selectedSub'])
 <template>
   <div class="content-frame">
     <div class="content">
+      <div v-if="selectedSub === '작성한 게시물'">
+        <WritePostList />
+      </div>
       <div v-if="selectedSub === '북마크한 게시물'">
         <BookmarkList />
       </div>
       <div v-if="selectedSub === '좋아요한 게시물'">
         <LikeList />
+      </div>
+      <div v-if="selectedSub === '신고된 게시물'">
+        <ReportedPostList />
       </div>
       <div v-if="selectedSub === '작성한 댓글'">
         <CommentList />
