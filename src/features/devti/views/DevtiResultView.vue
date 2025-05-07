9963e74011ed6cad0565c11458fdfa473ca76a78
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-07 11:47:50 +0900

    [feat #93] DevTI 공유 라우팅 수정

--- src/features/devti/views/DevtiResultView.vue
+++ src/features/devti/views/DevtiResultView.vue
@@ -14,14 +14,14 @@ onMounted(async () => {
 
     if (!devti) {
       alert('아직 DevTI 결과가 없습니다. 먼저 테스트를 진행해주세요.')
-      router.push('/mypage/devti/solve')
+      await router.push('/mypage/devti/solve')
     } else {
       resultType.value = devti
     }
   } catch (err) {
     alert('결과를 불러오는 데 실패했습니다.')
     console.error(err)
-    router.push('/mypage/devti/solve')
+    await router.push('/mypage/devti/solve')
   }
 })
 </script>
